8565367c916161d45c8a4d4be9409f7bd34acd47
yonaries <yonatandejene001@gmail.com>
2022-10-08 13:38:00 +0300

    lesson 4

---
+++ easy/tournment_winner.py
@@ -0,0 +1,24 @@
+arr = [['html', 'c#'], ['c#', 'python'], ['python', 'html']]
+res = [0, 0, 1]
+
+
+def pick_winner(competitions, results):
+    currentBestTeam = ""
+    scores = {currentBestTeam: 0}
+
+    for idx, comp in enumerate(competitions):
+        homeTeam, awayTeam = comp
+        winningTeam = homeTeam if results[idx] == 1 else awayTeam
+        updateScores(winningTeam, 3, scores)
+
+        if scores[winningTeam] > scores[currentBestTeam]:
+            currentBestTeam = winningTeam
+
+    return currentBestTeam
+
+
+def updateScores(team, point, scores):
+    if team not in scores:
+        scores[team] = 0
+
+    scores[team] += point
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from easy.two_sum import two_sum, arr
 from easy.valid_subsequence import validate_subsequence, arr2, arr1
 from easy.sorted_square_array import sort_square, arr
+from easy.tournment_winner import arr, res, pick_winner
 
-print(two_sum(arr, 10))
-print(validate_subsequence(arr1, arr2))
-print(sort_square(arr))
+# print(two_sum(arr, 10))
+# print(validate_subsequence(arr1, arr2))
+# print(sort_square(arr))
+print(pick_winner(arr, res))
